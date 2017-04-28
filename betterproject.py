25c2f172de106c5a639403fb4a0b400980ecf499
Sarah <slefkow2@binghamton.edu>
2017-04-27 22:59:19 -0400

    replaced with modulus

--- betterproject.py
+++ betterproject.py
@@ -2,7 +2,6 @@ import tkinter
 from PIL import Image, ImageTk
 
 #model
-
 class Arrow:
 
     def __init__(self, window, color='red', direction='left',X=0):
@@ -22,61 +21,24 @@ class Arrow:
         self.X = X
 
     def click(self, event):
-        room = [self.window.wall1, self.window.wall2, self.window.wall3, self.window.wall4]
         if self.direction == 'left':
-            if self.X == 0:
-                self.window.canvas.create_image(400,300,image=room[1])
-                arrow1 = Arrow(self.window,X=1)
-                arrow2 = Arrow(self.window,direction='right',X=1)
-                self.window.canvas.pack()
-            elif self.X == 1:
-                self.window.canvas.create_image(400,300,image=room[2])
-                arrow1 = Arrow(self.window,X=2)
-                arrow2 = Arrow(self.window,direction='right',X=2)
-                self.window.canvas.pack()
-            elif self.X == 2:
-                self.window.canvas.create_image(400,300,image=room[3])
-                arrow1 = Arrow(self.window,X=3)
-                arrow2 = Arrow(self.window,direction='right',X=3)
-                self.window.canvas.pack()
-            elif self.X == 3:
-                self.window.canvas.create_image(400,300,image=room[0])
-                arrow1 = Arrow(self.window,X=0)
-                arrow2 = Arrow(self.window,direction='right',X=0)
-                self.window.canvas.pack()
+            self.window.canvas.itemconfig(self.window.currentimage,image=self.window.walls[(self.X-1)%len(self.window.walls)])
+            self.window.arrow1.X=(self.X-1)%len(self.window.walls)
+            self.window.arrow2.X=(self.X-1)%len(self.window.walls)
         else:
-            if self.X == 0:
-                self.window.canvas.create_image(400,300,image=room[3])
-                arrow1 = Arrow(self.window,X=3)
-                arrow2 = Arrow(self.window,direction='right',X=3)
-                self.window.canvas.pack()
-            elif self.X == 1:
-                self.window.canvas.create_image(400,300,image=room[0])
-                arrow1 = Arrow(self.window,X=0)
-                arrow2 = Arrow(self.window,direction='right',X=0)
-                self.window.canvas.pack()
-            elif self.X == 2:
-                self.window.canvas.create_image(400,300,image=room[1])
-                arrow1 = Arrow(self.window,X=1)
-                arrow2 = Arrow(self.window,direction='right',X=1)
-                self.window.canvas.pack()
-            elif self.X == 3:
-                self.window.canvas.create_image(400,300,image=room[2])
-                arrow1 = Arrow(self.window,X=2)
-                arrow2 = Arrow(self.window,direction='right',X=2)
-                self.window.canvas.pack()
-
+            self.window.canvas.itemconfig(self.window.currentimage,image=self.window.walls[(self.X+1)%len(self.window.walls)])
+            self.window.arrow1.X=(self.X+1)%len(self.window.walls)
+            self.window.arrow2.X=(self.X+1)%len(self.window.walls)
 #view
 class Window:
     def __init__(self):
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(self.window,width=800,height=600)
         self.canvas.grid()
-        self.wall1 = tkinter.PhotoImage(file = "C:/Users/sarah/OneDrive/Documents/CS 110/BalconyChairs.gif")
-        self.wall2 = tkinter.PhotoImage(file = "C:/Users/sarah/OneDrive/Documents/CS 110/BalconyDoortoBdR.gif")
-        self.wall3 = tkinter.PhotoImage(file = "C:/Users/sarah/OneDrive/Documents/CS 110/BRTV.gif")
-        self.wall4 = tkinter.PhotoImage(file = "C:/Users/sarah/OneDrive/Documents/CS 110/BalconyWall.gif")
-        self.canvas.create_image(400,300,image=self.wall1)
+        basepath = "C:/Users/sarah/OneDrive/Documents/CS 110/"
+        paths = ["BalconyChairs.gif", "BalconyDoortoBdr.gif", "BRTV.gif", "BalconyWall.gif"]
+        self.walls = [tkinter.PhotoImage(file = basepath + path) for path in paths]
+        self.currentimage = self.canvas.create_image(400,300,image=self.walls[0])
         self.arrow1 = Arrow(self)
         self.arrow2 = Arrow(self,direction='right')
         self.canvas.pack()
