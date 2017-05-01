3310d034500b33c5d322edfc4454f7f0923b8bcd
amandaripley <aripley1@binghamton.edu>
2017-04-30 21:20:11 -0400

    Update betterproject.py

--- betterproject.py
+++ betterproject.py
@@ -3,6 +3,10 @@ from tkinter import simpledialog, messagebox
 from PIL import Image, ImageTk
 import winsound
 
+cluesfound = 0
+foundkey = 0
+
+
 #model
 class Arrow:
     def __init__(self, window, color='red', direction='left',X=0):
@@ -30,6 +34,13 @@ class Arrow:
             self.window.canvas.itemconfig(self.window.currentimage,image=self.window.walls[(self.X+1)%len(self.window.walls)])
             self.window.arrow1.X=(self.X+1)%len(self.window.walls)
             self.window.arrow2.X=(self.X+1)%len(self.window.walls)
+        if self.X == 0:
+            self.laptop = Laptop(self.window)
+        else:
+            try:
+                self.window.canvas.delete(self.laptop.idnumber)
+            except:
+                 pass
         if self.X == 1:
             self.speaker = Speaker(self.window)
         else:
@@ -37,13 +48,24 @@ class Arrow:
                 self.window.canvas.delete(self.speaker.idnumber)
             except:
                 pass
-        if self.X == 2:
+        if self.X == 3:
+            self.waterbottle = WaterBottle(self.window)
+        else:
+            try:
+                self.window.canvas.delete(self.waterbottle.idnumber)
+            except:
+                pass
+        if self.X == 2 and cluesfound == 3:
             self.key = Key(self.window)
         else:
             try:
                 self.window.canvas.delete(self.key.idnumber)
             except:
                 pass
+        if self.X == 2 and cluesfound == 3 and foundkey ==1:
+            self.door = Door(self.window)
+
+
 
 class Speaker:
     def __init__(self, window):
@@ -56,12 +78,61 @@ class Speaker:
         self.window.canvas.tag_bind(self.idnumber, "<Button-1>", self.click)
 
     def click(self,event):
-        winsound.PlaySound("C:/Users/sarah/OneDrive/Documents/CS 110/Believer.wav",winsound.SND_FILENAME)
+        winsound.PlaySound("C:/Users/aripl/Documents/college/CS 110/project1/Escape_the_Room/Believer.wav",winsound.SND_FILENAME)
         clue1 = simpledialog.askstring("Input", "What is the name of this song?")
         if clue1 == "Believer":
             messagebox.showinfo("You did it!", "Congratulations! You found a clue!")
+            global cluesfound
+            cluesfound += 1
+            self.window.canvas.delete(self.idnumber)
+            if cluesfound == 3:
+                messagebox.showinfo("Input","You've found all the clues! Now you can look for the key to open the door!")
+
         else:
             messagebox.showerror("That's not it!", "Nope! Try again.")
+class Laptop:
+    def __init__(self,window):
+        self.window = window
+        self.a = 600
+        self.b = 350
+        self.c = 640
+        self.d = 370
+        self.idnumber = self.window.canvas.create_rectangle(self.a,self.b,self.c,self.d, outline = '')
+        self.window.canvas.tag_bind(self.idnumber, "<Button-1>", self.click)
+
+    def click(self,event):
+        clue2 = simpledialog.askstring("Input", "What is the name of the game you are playing right now?")
+        if clue2 == "Escape the Room":
+            messagebox.showinfo("You did it!", "Congratulations! You found a clue!")
+            global cluesfound
+            cluesfound += 1
+            self.window.canvas.delete(self.idnumber)
+            if cluesfound == 3:
+                messagebox.showinfo("Input","You've found all the clues! Now you can look for the key to open the door!")
+        else:
+            messagebox.showerror("That's not it!", "Nope! Try again")
+
+class WaterBottle:
+    def __init__(self,window):
+        self.window = window
+        self.a = 160
+        self.b = 320
+        self.c = 170
+        self.d = 350
+        self.idnumber = self.window.canvas.create_rectangle(self.a, self.b, self.c, self.d, outline = '')
+        self.window.canvas.tag_bind(self.idnumber, "<Button-1>", self.click)
+
+    def click(self, event):
+        clue3 = simpledialog.askstring("Input", "What did you just click on?")
+        if clue3 == "Water Bottle":
+            global cluesfound
+            cluesfound += 1
+            messagebox.showinfo("You did it!", "Congratulation! You found a clue!")
+            self.window.canvas.delete(self.idnumber)
+            if cluesfound == 3:
+                messagebox.showinfo("Input","You've found all the clues! Now you can look for the key to open the door!")
+        else:
+            messagebox.showerror("That's not it!", "Nope! Try again")
 
 class Key:
     def __init__(self,window):
@@ -74,7 +145,23 @@ class Key:
         self.window.canvas.tag_bind(self.idnumber, "<Button-1>", self.click)
 
     def click(self,event):
-            messagebox.showinfo("Congratulations!","You've found the key!")
+            messagebox.showinfo("Congratulations!","You've found the key! Now use the door to escape!")
+            global foundkey
+            foundkey = 1
+
+class Door:
+    def __init__(self,window):
+        self.window = window
+        self.a = 150
+        self.b = 275
+        self.c = 240
+        self.d = 400
+        self.idnumber = self.window.canvas.create_rectangle(self.a, self.b, self.c, self.d, outline = '')
+        self.window.canvas.tag_bind(self.idnumber, "<Button-1>", self.click)
+    def click(self, event):
+        messagebox.showinfo("Congratulations!", "You have escaped!!")
+        global cluesfound
+        cluesfound += 1
 
 #view
 class Window:
@@ -83,7 +170,7 @@ class Window:
         self.window.wm_title("Escape the Room")
         self.canvas = tkinter.Canvas(self.window,width=800,height=600)
         self.canvas.grid()
-        basepath = "C:/Users/sarah/OneDrive/Documents/CS 110/"
+        basepath = "C:/Users/aripl/Documents/college/CS 110/project1/Escape_the_Room/"
         paths = ["image1_3_.gif", "image3.gif","image4.gif", "image2.gif"]
         self.walls = [tkinter.PhotoImage(file = basepath + path) for path in paths]
         self.currentimage = self.canvas.create_image(400,300,image=self.walls[0])
@@ -95,7 +182,8 @@ class Window:
         self.window.mainloop()
 
 #controller
-def main():
-    window = Window()
-    window.mainLoop()
-main()
+#def main():
+    #window = Window()
+    #window.mainLoop()
+
+#main()
